dc3126451af4aea80fe67a1635a8a8e7138f16d2
JeromelNapal <jeromelnapal10@gmail.com>
2025-01-28 14:05:21 +0000

    Done adding output

---
+++ Primitives.java
@@ -0,0 +1,16 @@
+public class Primitives {
+    public static void main(String[] args) {
+        char h = 'H';
+        int three = 3;
+        byte one = 1;
+        short zero =0;
+        float twoPointZero =2.0f;
+        boolean isTrue = true;
+
+
+        String output = "" + h + three + one + one + zero + " " + "w" + zero + "r" + one + "d" + " " + twoPointZero + " " + isTrue;
+
+        System.out.println(output);
+
+    }
+}
